fa0eeca6f1aa96016007541cba2d6c7f869f3244
Theam-Dev <phearakroun@gmail.com>
2023-07-19 20:23:18 +0700

    H Step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Stepper from 'react-stepper-horizontal';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [steps] = useState([
+    { title: 'Step 1' },
+    { title: 'Step 2' },
+    { title: 'Step 3' },
+  ]);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    address: '',
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [name]: value,
+    }));
+  };
+  const handleNext = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+  const handlePrevious = () => {
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted values:', inputValues);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Stepper
+        steps={steps}
+        activeStep={currentStep}
+        activeColor="#007bff"
+        completeColor="#28a745"
+      />
+      <div className='container'>
+      <form onSubmit={handleSubmit}>
+        {currentStep === 0 && (
+          <div className='form-group'>
+            <label htmlFor="name">Name:</label>
+            <input  className='form-control' type="text" name="name" value={inputValues.name} onChange={handleInputChange} />
+          </div>
+        )}
+        {currentStep === 1 && (
+          <div className='form-group'>
+            <label htmlFor="email">Email:</label>
+            <input  className='form-control' type="email" name="email" value={inputValues.email} onChange={handleInputChange} />
+          </div>
+        )}
+        {currentStep === 2 && (
+          <div className='form-group'>
+            <label htmlFor="address">Address:</label>
+            <input className='form-control' type="text" name="address" value={inputValues.address} onChange={handleInputChange} />
+          </div>
+        )}
+        <div>
+          {currentStep > 0 && (
+            <button className='btn btn-primary mt-4 me-4' type="button" onClick={handlePrevious}>
+              Previous
+            </button>
+          )}
+          {currentStep < steps.length - 1 ? (
+            <button className='btn btn-primary mt-4 me-4' type="button" onClick={handleNext}>
+              Next
+            </button>
+          ) : (
+            <button className='btn btn-primary mt-4' type="submit">Submit</button>
+          )}
+        </div>
+      </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
